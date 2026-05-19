46a9e29c3930dd34995262fe13506a004cac3f1d
Erick Vinicius Rodrigues <erickafram08@gmail.com>
2026-05-19 10:21:35 -0300

    Implementação de questionários dinâmicos e override de competência

--- app/Http/Controllers/ProcessoController.php
+++ app/Http/Controllers/ProcessoController.php
@@ -1184,7 +1184,21 @@ class ProcessoController extends Controller
                         $fpdi->useTemplate($template);
                     }
                 } catch (\Exception $e) {
-                    \Log::warning('Erro ao adicionar PDF na íntegra: ' . ($item['label'] ?? 'desconhecido'), [
+                    // Recria o FPDI para evitar estado corrompido após erro de parser
+                    $outputAtual = $fpdi->Output('S');
+                    $tempRecuperacao = storage_path('app/temp_integra_recuperacao.pdf');
+                    file_put_contents($tempRecuperacao, $outputAtual);
+                    $fpdi = new \setasign\Fpdi\Fpdi();
+                    $recPageCount = $fpdi->setSourceFile($tempRecuperacao);
+                    for ($i = 1; $i <= $recPageCount; $i++) {
+                        $template = $fpdi->importPage($i);
+                        $size = $fpdi->getTemplateSize($template);
+                        $fpdi->AddPage($size['orientation'], [$size['width'], $size['height']]);
+                        $fpdi->useTemplate($template);
+                    }
+                    @unlink($tempRecuperacao);
+
+                    \Log::warning('Erro ao adicionar PDF na íntegra (pulado): ' . ($item['label'] ?? 'desconhecido'), [
                         'erro' => $e->getMessage()
                     ]);
                 }
